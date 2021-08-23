209424c412f9d2e8f54775291005b4db377b805e
ak98 <ak98@DESKTOP-FR19AA5>
2021-08-23 10:53:14 +0200

    Add project files.

---
+++ IthsLabb1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IthsLabb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
