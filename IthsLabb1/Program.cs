a7fb83860fcaa9349943e6000281e7f6ebe3e18d
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-08-30 16:53:11 +0200

    skit

--- IthsLabb1/Program.cs
+++ IthsLabb1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Drawing;
 
 namespace IthsLabb1
 {
@@ -8,13 +8,25 @@ namespace IthsLabb1
     {
         static void Main(string[] args)
         {
-            /*Mata in en sträng, siffror och bokstäver.
-            Strängen ska sökas igenom och markera där strängen börjar och slutar på samma siffra utan att bli avbruten av en bokstav.*/
+            //Mata in en sträng, siffror och bokstäver. Strängen ska sökas igenom och markera där strängen börjar och slutar på samma siffra utan att bli avbruten av en bokstav.
 
+            
+            //Be användaren mata in en textsträng
             Console.WriteLine("Mata in valfri sträng av bokstäver och siffror");
-            string input = Console.ReadLine();
+            string input = "29535123p48723487597645723645";
+
+            
+
+            char[] inputChar = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                inputChar[i] = input[i];
+                Console.WriteLine(input);
+            }
 
 
+    
 
             Console.ReadKey();
         }
