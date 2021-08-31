2c73357232621015f74db4d9be5c303cc2ad0a4b
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-08-31 14:23:56 +0200

    hitta matchande tal + summan klart

--- IthsLabb1/Program.cs
+++ IthsLabb1/Program.cs
@@ -9,24 +9,58 @@ namespace IthsLabb1
         static void Main(string[] args)
         {
             //Mata in en sträng, siffror och bokstäver. Strängen ska sökas igenom och markera där strängen börjar och slutar på samma siffra utan att bli avbruten av en bokstav.
+            //Hitta ett sätt att spara flera tal/strängar
+
+
+            //Om inte, kolla nästa
+            //Kolla från nästa tecken och framåt
+
+            //Se om tecknet är samma som ifrån det yttre
+            //Om samma tecken påträffas, spara
 
             
+
             //Be användaren mata in en textsträng
             Console.WriteLine("Mata in valfri sträng av bokstäver och siffror");
             string input = "29535123p48723487597645723645";
+            long sum = 0;
 
-            
-
-            char[] inputChar = new char[input.Length];
-
+            //Loopa igenom alla tecken
             for (int i = 0; i < input.Length; i++)
             {
-                inputChar[i] = input[i];
-                Console.WriteLine(input);
+                //Kolla om tecknet är ett tal
+                //Om det inte är ett tal, avbryt och gå till nästa
+                if (char.IsNumber(input[i]) == false)
+                {
+                    continue;
+                }
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    //Kolla om tecknet är ett tal
+                    if (char.IsNumber(input[j]) == true)
+                    {
+                        if (input[i] == input[j])
+                        {
+                            string numberMatch = input.Substring(i, j - i + 1);
+                            Console.WriteLine("");
+                            Console.WriteLine(numberMatch);
+                            long result;
+                            long.TryParse(numberMatch, out result);
+                            sum += result;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
 
+            Console.WriteLine($"Summan av delsträngarna är {sum}");
 
-    
+
 
             Console.ReadKey();
         }
